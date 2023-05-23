9581d173c35e4cb2424043f025120fe62c23dba9
DaniFdezCab <danfercab@alum.us.es>
2023-05-23 09:56:10 +0200

    FinalVersion

--- src/main/java/AISS/GitHubMiner/service/ProjectService.java
+++ src/main/java/AISS/GitHubMiner/service/ProjectService.java
@@ -37,7 +37,7 @@ public class ProjectService {
         String uri = "https://api.github.com/repos/" + owner + "/" + repo;
 
         HttpHeaders headers = new HttpHeaders();
-        headers.set("Authorization", "Bearer " + "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");
+        headers.set("Authorization", "Bearer " + "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");
 
         HttpEntity<Project> request = new HttpEntity<>(null, headers);
         //send
